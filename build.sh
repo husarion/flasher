e830a6b106cc4cde2c56a31147692b2e9a3414b7
Krystian Dużyński <krystian.duzynski@gmail.com>
2016-09-12 11:50:50 +0200

    fix

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash -e
-if [ ! -z "$BOOTLOADER_DIR" ]; then
-	export BOOTLOADER_DIR=$(readlink -f $BOOTLOADER_DIR)
-fi
+# if [ ! -z "$BOOTLOADER_DIR" ]; then
+	# export BOOTLOADER_DIR=$(readlink -f $BOOTLOADER_DIR)
+# fi
 
-./gen_bootloader_files.sh
+# ./gen_bootloader_files.sh
 
 mkcd() {
 	mkdir -p "$1"
 	cd "$1"
 }
 
-(mkcd build/amd64-linux && cmake -DEMBED_BOOTLOADERS=1 ../.. -B. && make) &
-(mkcd build/i386-linux && cmake -DEMBED_BOOTLOADERS=1 -DX86=1 ../.. -B. && make) &
-(mkcd build/win && cmake -DEMBED_BOOTLOADERS=1 -DWIN32=1 -DCROSS=1 ../.. -B. && make) &
-wait
+mkcd build/amd64-linux && cmake -DEMBED_BOOTLOADERS=0 ../.. -B. && make
+mkcd build/i386-linux && cmake -DEMBED_BOOTLOADERS=0 -DX86=1 ../.. -B. && make
+mkcd build/win && cmake -DEMBED_BOOTLOADERS=0 -DWIN32=1 -DCROSS=1 ../.. -B. && make
