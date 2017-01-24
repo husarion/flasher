17cd2bea383a475e54ac55750483986f9417112b
Michał Zieliński <michal@zielinscy.org.pl>
2017-01-24 16:56:24 +0100

    hack compiling for i386 without multilib

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ mkcd() {
 }
 
 (mkcd build/amd64-linux && cmake -DEMBED_BOOTLOADERS=0 ../.. -B. && make)
-(mkcd build/i386-linux && cmake -DEMBED_BOOTLOADERS=0 -DX86=1 ../.. -B. && make)
+(mkcd build/i386-linux && cmake -DEMBED_BOOTLOADERS=0 -DX86=1 -DMULTILIB_HACK=i586-linux-gnu//4.9 ../.. -B. && make)
 (mkcd build/win && cmake -DEMBED_BOOTLOADERS=0 -DWIN32=1 -DCROSS=1 ../.. -B. && make)
 (mkcd build/rpi-linux && cmake -DCMAKE_TOOLCHAIN_FILE=../../rpitoolchain.cmake ../.. && make)
